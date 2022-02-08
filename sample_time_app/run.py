27d99a8f8844cb68e250c2dc861ba5a12292223c
Saurabh Paul <paul.saurabhpaul@yahoo.co.in>
2022-02-08 05:58:07 -0500

    initial commit

--- sample_time_app/run.py
+++ sample_time_app/run.py
@@ -1,4 +1,6 @@
 from flask import Flask
+import datetime
+import os
 app = Flask(__name__)
 
 
@@ -6,7 +8,13 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello world!'
 
+@app.route('/time')
+def getTime():
+    return datetime.datetime.now().strftime("%m/%d/%Y, %H:%M:%S")
 
-app.run(host='0.0.0.0',
-        port=8080,
-        debug=True)
+if __name__ == "__main__":
+    print("Initiating Flask App")
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True,host='0.0.0.0',port=port)
+
+
